1c08b5696a199ca94d72b5a59b40364c1377ec96
vitviki <vtyagi1991@gmail.com>
2024-05-28 21:17:51 +0530

    Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({ url: "", summary: "" });
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
+  const handleCopy = (URL) => {
+    setCopied(URL);
+    navigator.clipboard.writeText(URL);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  return (
+    <section className="mt-16 w-full max-w-3xl">
+      <div className="flex flex-col gap-2 w-full">
+        {/* Search box */}
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL for an article"
+            className="url_input peer"
+            value={article.url}
+            required
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            &#8617;
+          </button>
+        </form>
+        {/* Search history */}
+        <div className="flex flex-col overflow-y-auto max-h-60 gap-1">
+          {allArticles.map((item, idx) => (
+            <div
+              className="link_card"
+              key={`link-${idx}`}
+              onClick={() => setArticle(item)}
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt="copy btn"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Result */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img
+            src={loader}
+            alt="fetching"
+            className="w-20 h-20 object-contain"
+          />
+        ) : error ? (
+          <p>
+            Well, that wasn't supposed to happen....
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
